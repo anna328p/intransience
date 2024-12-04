80552dbc0df2b319d71aad0b2666d343a50a184b
Anna Kudriavtsev <anna328p@gmail.com>
2024-12-04 17:44:14 -0600

    add docs

--- flake.nix
+++ flake.nix
@@ -1,10 +1,24 @@
 {
 	description = "Intransience";
 
-	outputs = { ... }: {
-		nixosModules = rec {
-			intransience = ./default.nix;
-			default = intransience;
-		};
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+		systems.url = "github:nix-systems/default";
 	};
+
+	outputs = { nixpkgs, systems, ... }:
+		let
+			eachSystem = nixpkgs.lib.genAttrs (import systems);
+		in {
+			packages = eachSystem (system: let
+				pkgs = nixpkgs.legacyPackages.${system};
+			in {
+				docs = pkgs.callPackage ./generate-docs.nix { };
+			});
+
+			nixosModules = rec {
+				intransience = ./default.nix;
+				default = intransience;
+			};
+		};
 }
---
+++ generate-docs.nix
@@ -0,0 +1,55 @@
+{ pkgs, lib, ... }:
+
+let
+	im = lib.evalModules {
+		modules = [
+			./default.nix
+			{ _module.check = false; }
+		];
+	};
+
+	doc = pkgs.nixosOptionsDoc {
+		inherit (im) options;
+
+		warningsAreErrors = false;
+
+		transformOptions = opt:
+			if opt.name == "_module.args"
+				then opt // { visible = false; }
+				else opt;
+	};
+
+	md = pkgs.runCommand "options-doc.md" {} ''
+		cat ${doc.optionsCommonMark} >> $out
+	'';
+
+in
+	pkgs.stdenv.mkDerivation {
+		pname = "options-doc";
+		version = "0";
+
+		dontUnpack = true;
+
+		nativeBuildInputs = [ pkgs.mdbook ];
+
+		buildPhase = ''
+			mkdir src
+
+			cat >src/SUMMARY.md <<-EOF
+			# Summary
+
+			- [Options](options.md)
+			EOF
+
+			mdbook init . \
+				--title="intransience options" \
+				--ignore=none
+
+			cp ${md} src/options.md
+
+			mkdir -p $out
+			mdbook build -d $out
+		'';
+
+		dontInstall = true;
+	}
